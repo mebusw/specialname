4f0384ab85c3eafa742bfec89464893049721cde
JackyShen <mebusw@163.com>
2016-11-03 09:27:52 +0800

    set filepath for /static assets

--- specialname/settings.py
+++ specialname/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = 'static/'
+
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
