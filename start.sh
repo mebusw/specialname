605f5cfaeea8725718730b3de1dec8004c4d9716
JackyShen <mebusw@163.com>
2016-11-03 10:27:19 +0800

    update host config for production

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #! /bin/sh
 
-python manage.py runserver
+# python manage.py runserver
+
+cd /home/ec2-user/specialname
+sudo chown  -R root ./static/
+sudo chown  -R root ./db.sqlite3
+python manage.py collectstatic --noinput
+gunicorn --workers=2  mysite.wsgi:application &
+
