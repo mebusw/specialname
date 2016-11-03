32919ec73ae11dcc7f920d0f2b41f66853808265
JackyShen <mebusw@163.com>
2016-11-03 11:54:08 +0800

    update start script. add fixture file.

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
 
-# python manage.py runserver
+# python manage.py dumpdata --indent=2 auth.group auth.user specialname.product > fixtures.json
+# python manage.py loaddata fixtures.json
+
 
 cd /home/ec2-user/specialname
 sudo chown  -R root ./static/
-sudo chown  -R root ./db.sqlite3
+# sudo chown  -R root ./db.sqlite3
+sed -i  "s/\"sandbox/\"live/g" specialname/views/view_specialname.py
 python manage.py collectstatic --noinput
 gunicorn --workers=2  mysite.wsgi:application &
 
