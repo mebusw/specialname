a41c79ff645994d82ba1fce6735bbf888ddfc46c
JackyShen <mebusw@163.com>
2016-11-15 22:09:34 +0800

    TDD: name words combinations

--- specialname/test/test_trainer.py
+++ specialname/test/test_trainer.py
@@ -30,16 +30,18 @@ def choose_name_by_character(characters, data):
     names = data | select(
         lambda d: [d[0], d[1], d[3], sum(map(lambda x, y: int(x) * y, d[4:16], characters))]) | as_list
     ordered = sorted(names, key=lambda n: n[3], reverse=True)
-    print ordered
+    # print ordered
     return ordered
 
 
-def choose_name_word_by_tone():
+def choose_name_word_by_tone(characters, english_name, data):
     """
     中文词组,英文名,英文名,英文名,权重,honorable,intellectual,elegant,agile,powerful,organized,lucky,precious,artistic,beautiful,reliable,free,,,,
     :return:
     """
-    return
+    names = data | where(lambda d: english_name in d[1:4]) | as_list
+    # print names
+    return map(lambda n: n[0], names)
 
 
 class TestTrainer(TestCase):
@@ -54,7 +56,7 @@ class TestTrainer(TestCase):
     def test_choose_name_by_character(self):
         """
             0------------3 , 4--------->
-           男名,拼音,权重,音调,honorable,intellectual,elegant,agile,powerful,organized,lucky,precious,artistic,beautiful,reliable,free
+           名,拼音,权重,音调,honorable,intellectual,elegant,agile,powerful,organized,lucky,precious,artistic,beautiful,reliable,free
         """
         data = (
             ('A', 'aaa', '', 9, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0),
@@ -77,3 +79,13 @@ class TestTrainer(TestCase):
         data = read_csv('name_words.txt')
         self.assertEqual(195, len(data))
 
+    def test_choose_name_word_by_tone(self):
+        data = (
+            '拓理,Tony,Terry,,,0,2,0,1,2,0,0,1,0,0,0,0,,,,'.split(','),
+            '卓屹,George,Gerald,,,0,1,0,0,2,0,2,1,0,0,0,0,,,,'.split(','),
+            '思奋,Stephen,Stanford,,,0,2,0,0,2,0,0,0,0,0,0,0,,,,'.split(','),
+        )
+        user_selected_chars = (1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0)
+        english_name = 'Gerald'
+        names = choose_name_word_by_tone(user_selected_chars, english_name, data)
+        self.assertEqual('卓屹', names[0])
