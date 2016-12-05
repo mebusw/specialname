7752e4bbad62e9cf7b6c79140785cdb89fc481d6
JackyShen <mebusw@163.com>
2016-12-05 16:17:49 +0800

    restart gunicorn

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@ cd /home/ec2-user/specialname
 sudo chown  -R root ./static/
 # sudo chown  -R root ./db.sqlite3
 # sed -i  "s/\"sandbox/\"live/g" specialname/views/view_specialname.py
+pkill gunicorn
 python manage.py collectstatic --noinput
 gunicorn --workers=2  mysite.wsgi:application &
 
